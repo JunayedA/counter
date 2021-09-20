2fd21b3ba39dc2dae6da54c0f15b87da1a8fdc5c
JunayedAhmed <junayedahmed11099@gmail.com>
2021-09-20 14:59:52 +0600

    display api

--- src/App.js
+++ src/App.js
@@ -1,15 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
-import { Component, useState } from 'react';
+import { Component, useEffect, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
       <Counter></Counter>
+      <ExternalUsers></ExternalUsers>
     </div>
   );
 }
 
+function ExternalUsers(){
+  const [users,setUsers] = useState([]);
+  useEffect(()=>{
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.json())
+    .then(data => setUsers(data))
+  },[])
+  return(
+    <div>
+      <h3>External users</h3>
+      {
+        users.map(user => <User name={user.name} email={user.email  }>{user.name}</User>)
+      }
+    </div>
+  )
+}
+
+function User(props){
+  return(
+    <div className="App-header">
+      <h2>Name: {props.name}</h2>
+      <p>Email: {props.email}</p>
+    </div>
+  )
+}
 function Counter(){
   const [count,setCount] = useState(0);
   const handleIncrease = () =>setCount(count+1);
